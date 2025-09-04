b9f0d100627158878d7fbad99c04a6c2572665ac
SofiaNechaeva <sonyarepman0803@gmail.com>
2025-09-04 12:44:37 +0300

    Update urls.py

--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -6,7 +6,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls'))
-    #на уровне проекта
 ]
 
 
@@ -17,5 +16,3 @@ if settings.DEBUG:
     urlpatterns += static(
         settings.STATIC_URL, document_root=settings.STATIC_ROOT
     )
-
-
